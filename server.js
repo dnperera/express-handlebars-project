0f09133a20d6c1fc2891044c18f2b1413d09d6f3
Melissa <mwebb1037@gmail.com>
2017-10-02 23:07:07 -0400

    server

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const express = require('express');
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize;
 const expressHandlebars = require('express-handlebars');
-const bodyParser = require(body-parser);
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.static('public'))
+app.engine('handlebars', handlebars({defaultLayout: 'main', partialsDir: __dirname + '/views/partials/'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+sequelize.sync().then(() => {
 
 
 
 
-
+}
 
 
 app.listen(3000, function(){
